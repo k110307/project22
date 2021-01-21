860608c90cac3592741d510d3bd5841bbc8f558a
k110307 <72075705+k110307@users.noreply.github.com>
2021-01-21 19:38:44 +0530

    Add files via upload

---
+++ package.js
@@ -0,0 +1,23 @@
+class Package {
+    constructor(x,y,width,height) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+
+        World.add(world, this.body);
+    }
+    display(){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        Push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTRE);
+        
+    }
+}
